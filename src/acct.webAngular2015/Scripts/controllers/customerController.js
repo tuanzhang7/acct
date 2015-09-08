03a006239d0fdb5bd755608c1c3470e9028fc2fd
tuanzhang <tuanzhang@gmail.com>
2015-09-08 23:49:52 +0800

    $promise succ set page title

--- src/acct.webAngular2015/Scripts/controllers/customerController.js
+++ src/acct.webAngular2015/Scripts/controllers/customerController.js
@@ -7,25 +7,23 @@
 
                 //$scope.customers = customerSrv.query({ start: 0, limit: 10 });
                 $scope.customers = null;
-                customerSrv.query({ start: 0, limit: 10 }).then(function (data) {
+                customerSrv.query({ start: 0, limit: 10 }).$promise.then(function (data) {
                     $scope.customers = data;
+                    $scope.page.setTitle('Customers');
                 });
-                $scope.page.setTitle('Customers');
+                
             }])
         .controller('CustomerDetailController', ['$scope', '$routeParams', '$location', 
             'customerSrv', 'invoiceSvc',
                 function ($scope, $routeParams, $location,customerSrv, invoiceSvc) {
 
-                    $scope.customer = customerSrv.detail({ id: $routeParams.id });
+                    //$scope.customer = customerSrv.detail({ id: $routeParams.id });
                     $scope.transactions = invoiceSvc.query({ id: $routeParams.id });
-
-
-                    customerSrv.detail({ id: $routeParams.id }).then(function (data) {
-                        console.log($scope.data.Name)
-                        $scope.page.setTitle($scope.data.Name);
+                    $scope.customer = null;
+                    customerSrv.detail({ id: $routeParams.id }).$promise.then(function (data) {
+                        $scope.customer = data;
+                        $scope.page.setTitle($scope.customer.Name);
                     });
-
-                    
 
                     angular.element(document).ready(function () {
                         $('#myTab a:first').tab('show');
--- src/acct.webAngular2015/Scripts/services/customerService.js
+++ src/acct.webAngular2015/Scripts/services/customerService.js
@@ -4,39 +4,32 @@
     var customerServiceModule = angular.module('customerServices', ['ngResource']);
 
     customerServiceModule.factory('customerSrv', function ($resource) {
-        function query() {
-            console.log("sss");
-            return $resource({
-                url: 'http://localhost:63267/api/Customer?start=:start&limit=:limit',
-                method: 'GET',
-                params: { start: '@start', limit: '@limit' },
-                isArray: true,
-                headers: { 'auth-token': 'admin 1qazxsw@' }
-            });
-        }
-        function detail() {
-            return $resource({
-                url: 'http://localhost:63267/api/Customer/:id',
-                method: 'GET',
-                params: { id: '@id' }
-            });
-        }
-        return {
-            query: query,
-            detail: detail
-        }
-        //return $resource('http://localhost:63267/api/Customer?start=:start&limit=:limit', {}, {
-        //    query: { method: 'GET', params: { start: '@start', limit: '@limit' }, isArray: true, headers: { 'auth-token': 'admin 1qazxsw@' } }
-        //})
+        var baseUrl = 'http://localhost:63267/api';
+
+        //function query() {
+        //    console.log("sss");
+        //    return $resource(baseUrl+'/Customer?start=:start&limit=:limit',{},{
+        //        method: 'GET',
+        //        params: { start: '@start', limit: '@limit' },
+        //        isArray: true,
+        //        headers: { 'auth-token': 'admin 1qazxsw@' }
+        //    });
+        //}
+        //return {
+        //    query: query
+        //}
+        return $resource(baseUrl + '/Customer?start=:start&limit=:limit', {}, {
+            query: { method: 'GET', params: { start: '@start', limit: '@limit' }, isArray: true, headers: { 'auth-token': 'admin 1qazxsw@' } }
+        })
     });
 
-    //customerServiceModule.factory('customerSrv', function ($resource) {
+    customerServiceModule.factory('customerSrv', function ($resource) {
         
-    //    //return $resource('http://localhost:63267/api/Customer/:id', {}, {
-    //    //    detail: { method: 'GET', params: { id: '@id' } },
-    //    //    update: { method: 'PUT', params: { id: '@id' } },
-    //    //    delete: { method: 'DELETE', params: { id: '@id' } }
-    //    //})
-    //});
+        return $resource('http://localhost:63267/api/Customer/:id', {}, {
+            detail: { method: 'GET', params: { id: '@id' } },
+            update: { method: 'PUT', params: { id: '@id' } },
+            delete: { method: 'DELETE', params: { id: '@id' } }
+        })
+    });
 
 })();
